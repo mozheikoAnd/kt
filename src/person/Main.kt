c8024544095ec14867090287b826130266843d16
Andrei <mail.ru>
2026-02-08 14:40:11 +0300

    4.1 Completed

--- src/person/Main.kt
+++ src/person/Main.kt
@@ -1,13 +1,17 @@
 package person
 
 fun main() {
-    val john = Person(name = "John", age = 18, height = 178, weight = 71.3)
+    val john = Person(name = "John", height = 178, weight = 71.3)
 //    john.name = "John"
 //    john.age = 18
 //    john.height = 178
 //    john.weight = 71.3
 
-    john.info()
-    john.run()
-    john.sayHello()
+//    john.info()
+//    john.run()
+//    john.sayHello()
+    john.age = 18
+    println(john.age)
+    john.age = 17
+    println(john.age)
 }
--- src/person/Person.kt
+++ src/person/Person.kt
@@ -2,10 +2,21 @@ package person
 
 class Person(
     private val name: String,
-    private val age: Int,
     private val height: Int,
     private val weight: Double
 ){
+    var age: Int = 0
+    set(value) {
+        if (value < field) {
+            println("Wrong age")
+        }else{
+            field = value
+        }
+    }
+        get() {
+            println("Question about age is not comfortable")
+            return field
+        }
     fun run(){
         repeat(10){
             println("$name is run $it")
